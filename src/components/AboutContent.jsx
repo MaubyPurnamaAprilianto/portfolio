ed8d1fb05f4972c913fa40ddc4cf48b13ef3d218
maubypurnama <maubypurnama010407@gmail.com>
2024-07-03 15:36:12 +0700

    fix about

--- src/components/AboutContent.jsx
+++ src/components/AboutContent.jsx
@@ -1,33 +1,33 @@
-import { Link } from "react-router-dom";
-import "./AboutContentStyle.css";
-
 import React from "react";
-import React1 from "../assets/react1.jpg";
-import React2 from "../assets/react2.webp";
+import { Link } from "react-scroll";
+import { FaArrowDown } from "react-icons/fa"; // You can import other icons as needed
+import "./AboutContentStyle.css";
+import Mauby from "../assets/Mauby.png";
 
 const AboutContent = () => {
   return (
-    <div className="about">
+    <div className="about" id="about">
+      <div className="right">
+        <div className="img-container">
+          <div className="">
+            <img src={Mauby} className="img" alt="Mauby" />
+          </div>
+        </div>
+      </div>
       <div className="left">
         <h1>Who Am I?</h1>
         <p>
-          I'm a react front-end developer. I create responsive secure website
-          for my clients.
+          I'm a React front-end developer. I create responsive and secure websites for my clients.
         </p>
-        <Link to="#contact">
+        <Link
+          to="contact"
+          spy={true}
+          smooth={true}
+          offset={-70} // Adjust this offset as per your design
+          duration={500}
+        >
           <button className="btn">Contact</button>
         </Link>
-      </div>
-
-      <div className="right">
-        <div className="img-container">
-          <div className="img-stack top">
-            <img src={React1} className="img" alt="true" />
-          </div>
-          <div className="img-stack bottom">
-            <img src={React2} className="img" alt="true" />
-          </div>
-        </div>
       </div>
     </div>
   );
