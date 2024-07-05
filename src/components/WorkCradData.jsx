9c48345b8522ff331852de1097f0591b379306ea
maubypurnama <maubypurnama010407@gmail.com>
2024-07-05 21:53:24 +0700

    fix source

--- src/components/WorkCradData.jsx
+++ src/components/WorkCradData.jsx
@@ -3,58 +3,51 @@ import pro2 from "../assets/pro2.png";
 import pro3 from "../assets/pro3.png";
 import pro4 from "../assets/pro4.png";
 import pro5 from "../assets/pro5.png";
-import pro6 from "../assets/pro6.png"
+import pro6 from "../assets/pro6.png";
 import pro7 from "../assets/pro7.png";
 
 const ProjectCardData = [
   {
     imgsrc: pro1,
-    title: "Project Lading Page #1",
-    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam beatae quibusdam architecto voluptatem vero ipsa.",
+    title: "Project Landing Page #1",
+    text: "https://github.com/MaubyPurnamaAprilianto/landingpage.git",
     view: "https://maubyproject1.netlify.app/",
-    source: "",
   },
   {
     imgsrc: pro2,
-    title: "Project Lading Page #2",
-    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam beatae quibusdam architecto voluptatem vero ipsa.",
+    title: "Project Landing Page #2",
+    text: "https://github.com/MaubyPurnamaAprilianto/card-singgle-page.git",
     view: "https://maubyproject2.netlify.app/",
-    source: "",
   },
   {
     imgsrc: pro3,
     title: "Portfolio #1",
-    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam beatae quibusdam architecto voluptatem vero ipsa.",
+    text: "",
     view: "https://mauby-portfolio.netlify.app/",
-    source: "",
   },
   {
     imgsrc: pro4,
     title: "Project Multi Page #1",
-    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam beatae quibusdam architecto voluptatem vero ipsa.",
+    text: "https://github.com/MaubyPurnamaAprilianto/Furniture.git",
     view: "https://maubyfurniture.netlify.app/",
-    source: "",
   },
   {
     imgsrc: pro5,
     title: "Project Lomba",
-    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam beatae quibusdam architecto voluptatem vero ipsa.",
+    text: "",
     view: "https://piringnusantara.netlify.app/",
-    source: "",
   },
   {
     imgsrc: pro6,
     title: "Project Fullstack #1",
-    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam beatae quibusdam architecto voluptatem vero ipsa.",
+    text: "https://github.com/MaubyPurnamaAprilianto/BukaWaroeng.git",
     view: "https://maubyproject.000webhostapp.com/",
-    source: "",
   },
   {
     imgsrc: pro7,
     title: "Consume Api TheMovie",
-    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam beatae quibusdam architecto voluptatem vero ipsa.",
+    text: "https://github.com/MaubyPurnamaAprilianto/the-movie-Consume-api.git",
     view: "https://the-movie-consume-api.vercel.app/",
-    source: "",
   },
 ];
 
